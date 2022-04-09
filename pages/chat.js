1e0886545178c8f1dd57d1c4f09cba08bc26d7be
Gabriel <gabrielvitor.frasao@gmail.com>
2022-04-09 10:58:14 -0300

    send icon added

--- pages/chat.js
+++ pages/chat.js
@@ -2,11 +2,11 @@ import { Box, Button, TextField, Title, Text } from "../components/basics";
 import Image from "next/dist/client/image";
 import { useState } from "react";
 import { useRouter } from "next/dist/client/router";
+import Link from "next/link";
 
 export default function ChatPage() {
     const [message, setMessage] = useState(""),
-        [messagesList, setMessagesList] = useState([]),
-        router = useRouter();
+        [messagesList, setMessagesList] = useState([]);
 
     function updateMessagesList() {
         if (message == "") {
@@ -31,26 +31,40 @@ export default function ChatPage() {
                 <Box className="w-4/5 h-full md:h-auto md:aspect-video px-4 py-8 flex flex-col bg-slate-800 rounded-md">
                     <Header />
 
-                    <Box className=" p-4 flex-1 flex flex-col bg-slate-700 rounded-md">
+                    <Box className=" p-4 flex-1 flex flex-col bg-slate-700 rounded-md overflow-y-hidden">
                         <MessageList msg={messagesList} />
 
-                        <TextField
-                            value={message}
-                            placeholder="Insira sua mensagem aqui..."
-                            className="px-2 py-3 bg-slate-800 rounded-md"
-                            onChange={(event) => {
-                                setMessage(event.target.value);
-                            }}
-                            onKeyPress={(event) => {
-                                if (event.key != "Enter") {
-                                    return;
-                                }
-                                event.preventDefault();
+                        <Box className="flex">
+                            <TextField
+                                value={message}
+                                placeholder="Insira sua mensagem aqui..."
+                                className="px-2 py-3 flex-1 bg-slate-800 rounded-md"
+                                onChange={(event) => {
+                                    setMessage(event.target.value);
+                                }}
+                                onKeyPress={(event) => {
+                                    if (event.key != "Enter") {
+                                        return;
+                                    }
+                                    event.preventDefault();
+                                    updateMessagesList();
+                                    setMessage("");
+                                }}
+                            />
 
-                                updateMessagesList();
-                                setMessage("");
-                            }}
-                        />
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor"
+                                className="bi bi-send-fill w-8 ml-6 mr-2 fill-slate-300 cursor-pointer"
+                                viewBox="0 0 16 16"
+                                onClick={() => {
+                                    updateMessagesList();
+                                    setMessage("");
+                                }}
+                            >
+                                <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z" />
+                            </svg>
+                        </Box>
                     </Box>
                 </Box>
             </Box>
@@ -63,54 +77,52 @@ function Header() {
         <Box className="mb-6 flex justify-between items-center">
             <Title className="text-2xl font-semibold">Chat</Title>
 
-            <Button className="p-2 hover:bg-slate-600 rounded-md">
-                Logout
-            </Button>
+            <Link href="/" passHref={true}>
+                <a className="p-2 hover:bg-slate-600 rounded-md">Logout</a>
+            </Link>
         </Box>
     );
 }
 
 function MessageList(props) {
     return (
-        <>
-            <Box
-                tag="ul"
-                className="w-full h-full flex flex-col-reverse justify-start overflow-y-scroll"
-            >
-                {props.msg.map((el) => {
-                    return (
-                        <>
-                            <Box
-                                tag="li"
-                                key={el.id}
-                                className="my-1 py-1 hover:px-1 hover:bg-slate-600 rounded-md"
-                            >
-                                <Box className="flex items-center">
-                                    <Box className="relative w-8 aspect-square mr-2">
-                                        <Image
-                                            src={`https://github.com/slyCooper-n.png`}
-                                            alt="user picture"
-                                            layout="fill"
-                                            className="rounded-full"
-                                            priority
-                                        />
-                                    </Box>
-
-                                    <Text className="mr-2 text-slate-400">
-                                        {el.from}
-                                    </Text>
-
-                                    <Text className="text-xs text-slate-400">
-                                        {el.date}
-                                    </Text>
+        <Box
+            tag="ul"
+            className="w-full flex-1 flex flex-col-reverse justify-start overflow-y-scroll"
+        >
+            {props.msg.map((el) => {
+                return (
+                    <>
+                        <Box
+                            tag="li"
+                            key={el.id}
+                            className="my-1 py-1 hover:px-1 hover:bg-slate-600 rounded-md"
+                        >
+                            <Box className="flex items-center">
+                                <Box className="relative w-8 aspect-square mr-2">
+                                    <Image
+                                        src={`https://github.com/slyCooper-n.png`}
+                                        alt="user picture"
+                                        layout="fill"
+                                        className="rounded-full"
+                                        priority
+                                    />
                                 </Box>
 
-                                <Text className="mt-2">{el.text}</Text>
+                                <Text className="mr-2 text-slate-400">
+                                    {el.from}
+                                </Text>
+
+                                <Text className="text-xs text-slate-400">
+                                    {el.date}
+                                </Text>
                             </Box>
-                        </>
-                    );
-                })}
-            </Box>
-        </>
+
+                            <Text className="mt-2">{el.text}</Text>
+                        </Box>
+                    </>
+                );
+            })}
+        </Box>
     );
 }
